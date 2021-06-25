358f8d627138894ebe09f3218b617fe206c0101e
Akeem Morris <ilswakeem@gmail.com>
2021-06-24 19:04:44 -0500

    first commit

---
+++ src/Nav.js
@@ -0,0 +1,38 @@
+import React from "react";
+import logo from "./MarketMe/images/market-me-logo.png";
+import { Link } from "react-router-dom";
+
+const Nav = () => {
+  return (
+    <nav>
+      <div className="nav">
+        <div className="logo">
+          <img style={{ height: "45px" }} src={logo} alt=" Market Me logo" />
+        </div>
+        <div className="navigation">
+          <ul>
+            <Link to="/">
+              <li>Home</li>
+            </Link>
+          </ul>
+          <ul>
+            <Link to="/our-services">
+              <li>Services</li>
+            </Link>
+          </ul>
+          <ul>
+            <a href="#work">
+              <li>Work</li>
+            </a>
+          </ul>
+          <ul>
+            <a href="#contact">
+              <li>Contact</li>
+            </a>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+export default Nav;
---
+++ src/Services.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Services = () => {
+  return <main>Our Services</main>;
+};
+
+export default Services;
