1da37de05696457c5bbabc8934afb58c88816e35
Akeem Morris <ilswakeem@gmail.com>
2021-06-28 17:59:34 -0500

    added modal navigation center

--- src/Nav.js
+++ src/Nav.js
@@ -1,8 +1,11 @@
-import React from "react";
+/* eslint-disable no-unused-vars */
+import React, { useState } from "react";
 import logo from "./MarketMe/images/market-me-logo.png";
 import { Link } from "react-router-dom";
 
 const Nav = () => {
+  const [nav, isNav] = useState(false);
+
   return (
     <nav className="home-nav">
       <div className="nav">
@@ -34,12 +37,53 @@ const Nav = () => {
             </a>
           </ul>
         </div>
-        <div className="hamburger-cont">
+        <div
+          className="hamburger-cont"
+          onClick={() => {
+            isNav(!nav);
+          }}
+        >
           <div className="burger1"></div>
           <div className="burger2"></div>
           <div className="burger3"></div>
         </div>
       </div>
+      {nav && (
+        <div className="modal-nav">
+          <div
+            className="close-btn"
+            onClick={() => {
+              isNav(false);
+            }}
+          >
+            <i className="far fa-window-close"></i>
+          </div>
+          <div className="nav-area">
+            <div className="navi">
+              <ul>
+                <Link to="/">
+                  <li>Home</li>
+                </Link>
+              </ul>
+              <ul>
+                <Link to="/our-services">
+                  <li>Services</li>
+                </Link>
+              </ul>
+              <ul>
+                <a href="#work">
+                  <li>Work</li>
+                </a>
+              </ul>
+              <ul>
+                <a href="#contact">
+                  <li>Contact</li>
+                </a>
+              </ul>
+            </div>
+          </div>
+        </div>
+      )}
     </nav>
   );
 };
--- src/Services.js
+++ src/Services.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./MarketMe/images//market-me-logo.png";
 import { Link } from "react-router-dom";
 import OurServices from "./OurServices";
 import Footer from "./Footer";
 
 const Services = () => {
+  const [nav, isNav] = useState(false);
+
   return (
     <main>
       <nav className="services-nav" id="home">
@@ -41,12 +43,53 @@ const Services = () => {
               </Link>
             </ul>
           </div>
-          <div className="hamburger-cont">
+          <div
+            className="hamburger-cont"
+            onClick={() => {
+              isNav(!nav);
+            }}
+          >
             <div className="burger1"></div>
             <div className="burger2"></div>
             <div className="burger3"></div>
           </div>
         </div>
+        {nav && (
+          <div className="modal-nav">
+            <div
+              className="close-btn"
+              onClick={() => {
+                isNav(false);
+              }}
+            >
+              <i className="far fa-window-close"></i>
+            </div>
+            <div className="nav-area">
+              <div className="navi">
+                <ul>
+                  <Link to="/">
+                    <li>Home</li>
+                  </Link>
+                </ul>
+                <ul>
+                  <Link to="/our-services">
+                    <li>Services</li>
+                  </Link>
+                </ul>
+                <ul>
+                  <a href="#work">
+                    <li>Work</li>
+                  </a>
+                </ul>
+                <ul>
+                  <a href="#contact">
+                    <li>Contact</li>
+                  </a>
+                </ul>
+              </div>
+            </div>
+          </div>
+        )}
       </nav>
       <OurServices />
       <Footer />
