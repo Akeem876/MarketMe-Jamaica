4b5fbf0bcba18afa6f9e672894c72aa28e98ee6e
Akeem Morris <ilswakeem@gmail.com>
2021-06-29 10:35:21 -0500

    added effect on top btn

--- src/MainFirst.js
+++ src/MainFirst.js
@@ -1,9 +1,28 @@
-import React from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 import Nav from "./Nav";
 
 const MainFirst = () => {
+  const [visibility, setVisibility] = useState(false);
+  const shows = 500;
+  const handleScroll = () => {
+    if (window.pageYOffset > shows) {
+      if (!visibility) setVisibility(true);
+    } else {
+      if (visibility) setVisibility(false);
+    }
+  };
+  useEffect(() => {
+    if (shows) {
+      window.addEventListener("scroll", handleScroll);
+      // return () => {
+      //   window.removeEventListener("scroll", handleScroll);
+      // };
+    }
+  }, [handleScroll]);
   return (
     <section className="main-first" id="home">
       <div className="overlay">
@@ -18,13 +37,15 @@ const MainFirst = () => {
           </div>
         </div>
       </div>
-      <div className="to-top">
-        <a href="#home">
-          <button className="to-top-button">
-            <i className="fas fa-arrow-circle-up"></i>
-          </button>
-        </a>
-      </div>
+      {visibility && (
+        <div className="top-top">
+          <a href="#home">
+            <button className="to-top-button">
+              <i className="fas fa-arrow-circle-up"></i>
+            </button>
+          </a>
+        </div>
+      )}
     </section>
   );
 };
